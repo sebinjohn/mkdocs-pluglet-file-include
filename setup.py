1ed8cf503e04ed4f67fb388c235f63dcf1d01768
Sebin John <Sebin.John@cba.com.au>
2022-02-09 22:41:42 +1100

    Add build configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mkdocs-pluglet-file-include',
+    version='0.1.0',
+    description="include file contents using anchors",
+    packages=['mkdocs_pluglet_file_include'],
+    license='GPL3',
+    author='Sebin John',
+    author_email='sebin.john.sebin@gmail.com',
+    url='https://github.com/sebinjohn/mkdocs-pluglet-file-include'
+)
